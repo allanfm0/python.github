0f4bd10e3702adc8b0f30d665640d4420936ab4a
Allan <allanfelipedk@gmail.com>
2023-08-07 00:22:06 -0300

    day 1 beginner - working with variables in python to manage data

---
+++ day-1-beginner-working-with-variables-in-python-to-manage-data/day-1-printing-start.py
@@ -0,0 +1,28 @@
+#Printing in console:
+    #print("Hello World!")
+
+#Separates line:
+    #print("Hello World!\nHello World!")
+
+#Concatenation:
+    #print("Hello " + "Allan")
+
+#Python input function:
+    # print("Hello " + input("Whats your name? "))
+    # print(len(input("Whats your name? ")))
+
+#Python Variables:
+    # name = input("Whats your name? ")
+    # print(name)
+    # length = len(name)
+    # print(length)
+    # number = 123
+    # print(number)
+
+#Varibles naming
+    # user_name = input("Whats your name? ")
+    # print(user_name)
+
+    
+
+
