c19859d7be37a939a42d1b43b3dab6d00c84acdb
Allan <allanfelipedk@gmail.com>
2023-08-10 20:21:37 -0300

    day-3-beginner-control-flow-and-logical-operators

---
+++ day-3-beginner-control-flow-and-logical-operators/Multiple If Statements in Succession.py
@@ -0,0 +1,25 @@
+print("Welcome to rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("Whats your age? "))
+    if age < 12:
+        bill = 5
+        print(f"Please pay ${bill}.")
+    elif age <= 18:
+        bill = 7
+        print(f"Please pay ${bill}.")
+    else:
+        bill = 12
+        print(f"Please pay ${bill}.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+        print(f"Your final bill is {bill}!")
+    if wants_photo == "N":
+        print("")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
